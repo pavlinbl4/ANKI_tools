08815292eacddc5a0532d86b34d6bbbf5050c1f1
evgeniy <pavlinl4@gmail.com>
2022-09-28 15:52:58 +0300

    can crete only two fields in note

--- create_note.py
+++ create_note.py
@@ -3,9 +3,13 @@
 import random
 import genanki
 import csv
+from pathlib import Path
+import os
 
-new_words = '/Volumes/big4photo/Documents/ANKI/Saved translations - Saved translations.csv'
-anki_folder = "/Volumes/big4photo/Documents/ANKI"
+new_words = f'{Path().home()}/Downloads/Saved translations - Saved translations.csv'
+anki_folder = f"{Path().home()}/Documents/ANKI"
+
+os.makedirs(anki_folder, exist_ok=True)
 
 model_id = random.randrange(1 << 30, 1 << 31)
 print(model_id)
@@ -14,10 +18,17 @@ deck_id = random.randrange(1 << 30, 1 << 31)
 print(deck_id)
 
 my_model = genanki.Model(model_id,
-                         'Knowledge',
+                         'My English cards',
                          fields=[
-                             {'name': 'Question'},
-                             {'name': 'Answer'},
+                             {'name': 'Front'},
+                             {'name': 'Back'},
+                             # {'name': 'Grammar'},
+                             # {'name': 'Definition'},
+                             # {'name':'Sound'},
+                             # {'name':'Eng'},
+                             # {'name':'Synonyms'},
+                             # {'name':'Transcription'},
+                             # {'name':'Image'}
                          ],
                          templates=[
                              {
@@ -29,7 +40,7 @@ my_model = genanki.Model(model_id,
 
 my_deck = genanki.Deck(deck_id, "Knowledge")
 
-with open(new_words,'r') as csv_file:
+with open(new_words, 'r') as csv_file:
     words = csv.reader(csv_file)
     for line in words:
         my_note = genanki.Note(
@@ -39,15 +50,6 @@ with open(new_words,'r') as csv_file:
         my_deck.add_note(my_note)
 
         print(line)
-
-
-
-
-
-
-
-
-
 
 anki_questions_file = f'{anki_folder}/anki_question.apkg'
 genanki.Package(my_deck).write_to_file(anki_questions_file)
