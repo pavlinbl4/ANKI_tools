54c6858d83d6e92bbfa66548c12d39d6cd41a58a
evgeniy <pavlinl4@gmail.com>
2023-01-04 21:50:57 +0300

    remove html tags in line with regex

---
+++ optimise_exel_file/remove_tags.py
@@ -0,0 +1,17 @@
+import re
+
+clearn = re.compile('<.*?>|&([a-z0-9]+|#[0-9]{1,6}|#x[0-9a-f]{1,6});')
+
+
+def cleanhtml(raw_html):
+    cleantext = re.sub(clearn, '', raw_html)
+    return cleantext
+
+
+
+if __name__ == '__main__':
+    # line_to_clear = '<div><div><div><div><div><span style="color: rgba(26, 38, 51, 0.6);">ett</span></div></div></div></div></div>'
+    line_to_clear = '<span style="color: rgba(26, 38, 51, 0.6); background-color: rgb(255, 255, 255);">mozegett</span>'
+    print(cleanhtml(line_to_clear))
+
+
