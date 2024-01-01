7d0ef9f0ca14d2d5da008583668c5e250024c0d8
evgeniy <pavlinbl4@gmail.com>
2024-01-01 22:38:08 +0300

    added remove quoters

--- optimise_exel_file/remove_tags.py
+++ optimise_exel_file/remove_tags.py
@@ -5,13 +5,16 @@ clearn = re.compile('<.*?>|&([a-z0-9]+|#[0-9]{1,6}|#x[0-9a-f]{1,6});')
 
 def cleanhtml(raw_html):
     cleantext = re.sub(clearn, '', raw_html)
+
+    # remove quoters
+    cleantext = re.sub('\"', '', cleantext)
     return cleantext
 
 
 
 if __name__ == '__main__':
     # line_to_clear = '<div><div><div><div><div><span style="color: rgba(26, 38, 51, 0.6);">ett</span></div></div></div></div></div>'
-    line_to_clear = '<span style="color: rgba(26, 38, 51, 0.6); background-color: rgb(255, 255, 255);">mozegett</span>'
+    line_to_clear = '<span style="color: rgba(26, 38, 51, 0.6); background-color: rgb(255, 255, 255);">"mozegett"</span>'
     print(cleanhtml(line_to_clear))
 
 
