76b9ec2f17fb5ff5c321791f8f07f6939e9237b3
evgeniy <pavlinbl4@gmail.com>
2024-01-01 22:48:56 +0300

    added remove quoters, but not work

--- optimise_exel_file/remove_tags.py
+++ optimise_exel_file/remove_tags.py
@@ -7,14 +7,15 @@ def cleanhtml(raw_html):
     cleantext = re.sub(clearn, '', raw_html)
 
     # remove quoters
-    cleantext = re.sub('\"', '', cleantext)
+    cleantext = re.sub('“|”', '', cleantext)
     return cleantext
 
 
 
 if __name__ == '__main__':
     # line_to_clear = '<div><div><div><div><div><span style="color: rgba(26, 38, 51, 0.6);">ett</span></div></div></div></div></div>'
-    line_to_clear = '<span style="color: rgba(26, 38, 51, 0.6); background-color: rgb(255, 255, 255);">"mozegett"</span>'
+    # line_to_clear = '<span style="color: rgba(26, 38, 51, 0.6); background-color: rgb(255, 255, 255);">"mozegett"</span>'
+    line_to_clear ='“BAD ATTITUDE, the shirt proclaimed.”'
     print(cleanhtml(line_to_clear))
 
 
