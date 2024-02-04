3e2f02cc85b23e3bfac81c8639c76f4e19c8315f
evgeniy_MacBook <pavlinbl4@gmail.com>
2024-02-04 22:21:37 +0300

    refactoring

--- create_note.py
+++ create_note.py
@@ -2,7 +2,6 @@
 
 import random
 import genanki
-import csv
 from pathlib import Path
 import os
 
@@ -11,24 +10,26 @@ anki_folder = f"{Path().home()}/Documents/ANKI"
 
 os.makedirs(anki_folder, exist_ok=True)
 
-model_id = random.randrange(1 << 30, 1 << 31)
-print(model_id)
+# model_id = random.randrange(1 << 30, 1 << 31)
+model_id = 2137747653
+print(f"{model_id = }")
 
 deck_id = random.randrange(1 << 30, 1 << 31)
-print(deck_id)
+deck_id = 1780842558
+print(f"{deck_id = }")
 
 my_model = genanki.Model(model_id,
-                         'My English cards',
+                         'Python generated cards',
                          fields=[
                              {'name': 'Front'},
                              {'name': 'Back'},
-                             # {'name': 'Grammar'},
-                             # {'name': 'Definition'},
-                             # {'name':'Sound'},
-                             # {'name':'Eng'},
-                             # {'name':'Synonyms'},
-                             # {'name':'Transcription'},
-                             # {'name':'Image'}
+                             {'name': 'Grammar'},
+                             {'name': 'Definition'},
+                             {'name':'Sound'},
+                             {'name':'Eng'},
+                             {'name':'Synonyms'},
+                             {'name':'Transcription'},
+                             {'name':'Image'}
                          ],
                          templates=[
                              {
@@ -38,18 +39,16 @@ my_model = genanki.Model(model_id,
                              },
                          ])
 
-my_deck = genanki.Deck(deck_id, "Knowledge")
+my_deck = genanki.Deck(deck_id, "Knowledge with Python")
 
-with open(new_words, 'r') as csv_file:
-    words = csv.reader(csv_file)
-    for line in words:
-        my_note = genanki.Note(
-            model=my_model,
-            fields=[f'{line[2].lower()}', f'{line[3].lower()}'])
 
-        my_deck.add_note(my_note)
 
-        print(line)
+note = genanki.Note(
+  model=my_model,
+  fields=['Front text!', 'Back text!', 'Grammar text!', 'Definition text', 'Sound text', 'Eng text', 'Synonyms text', 'Transcription text', 'Image text'])
 
-anki_questions_file = f'{anki_folder}/anki_question.apkg'
+my_deck.add_note(note)
+
+
+anki_questions_file = f'{anki_folder}/anki_question_python.apkg'
 genanki.Package(my_deck).write_to_file(anki_questions_file)
