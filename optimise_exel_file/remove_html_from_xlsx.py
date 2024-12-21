ff19bccab9a7f228e487fccd5d79078495bf4a69
evgeniy <pavlinbl4@gmail.com>
2024-12-21 21:53:43 +0300

    changed path to anki files to icloud

--- optimise_exel_file/remove_html_from_xlsx.py
+++ optimise_exel_file/remove_html_from_xlsx.py
@@ -1,9 +1,13 @@
 import re
 from openpyxl import load_workbook
+from pathlib import Path
+
+
+def file_name_with_suffix(path_to_xlsx_file, suffix):
+    return f'{Path(path_to_xlsx_file).parent}/{Path(path_to_xlsx_file).stem}_{suffix}{Path(path_to_xlsx_file).suffix}'
 
 
 def remove_html_from_cells(path_to_xlsx_file):
-
     # Load the Excel file
     workbook = load_workbook(path_to_xlsx_file)
     sheet = workbook.active
@@ -19,9 +23,10 @@ def remove_html_from_cells(path_to_xlsx_file):
             if cell.value:
                 cell.value = remove_html_tags(str(cell.value))
     # Save the modified workbook
-    workbook.save('your_file_cleaned.xlsx')
+    workbook.save(file_name_with_suffix(path_to_xlsx_file, 'cleared'))
+
 
 if __name__ == '__main__':
+    # /Users/evgeniy/Library/Mobile Documents/com~apple~CloudDocs/ANKI
+    print(file_name_with_suffix((Path().home()/'Library/Mobile Documents/com~apple~CloudDocs/ANKI/All Decks.xlsx'), 'test_suffix'))
 
-
-    remove_html_from_cells('/Volumes/big4photo/Documents/ANKI/All Decks.xlsx')
