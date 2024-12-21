e4f554da68437f6c0998c372a4d93bbced8c13df
evgeniy <pavlinbl4@gmail.com>
2024-12-21 21:52:26 +0300

    changed path to anki files to icloud

--- optimise_exel_file/main_remove_tags_from_xlsx.py
+++ optimise_exel_file/main_remove_tags_from_xlsx.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+
 import openpyxl
 from remove_tags import cleanhtml
 from tkinter import filedialog, messagebox
@@ -15,7 +17,7 @@ def get_sheet_names(xlsx_file_data):
 
 def find_specific_cell(current_sheet):
     for row in range(8, current_sheet.max_row + 1):
-        for column in "BCDE":  # Here you can add or reduce the columns
+        for column in "ABCDEGH":  # Here you can add or reduce the columns
             cell_name: str = "{}{}".format(column, row)
             if current_sheet[cell_name].value is not None:
                 if "<" in current_sheet[cell_name].value:
@@ -25,6 +27,14 @@ def find_specific_cell(current_sheet):
                     current_sheet[cell_name].value = clean_value
 
 
+def iterate_cells(current_sheet):
+    # Iterate through each cell in columns A to I and remove HTML tags
+    for row in current_sheet.iter_rows(min_row=1, min_col=1, max_col=9):
+        for cell in row:
+            if cell.value:
+                cell.value = "$$$$$$$"
+
+
 def clear_from_tags(anki_excel_file):
     file_name = get_filename(anki_excel_file)
     xlsx_file_data = read_data(anki_excel_file)
@@ -32,17 +42,15 @@ def clear_from_tags(anki_excel_file):
     current_sheet = xlsx_file_data[sheet_names[0]]
     find_specific_cell(current_sheet)
 
-    # create file with new name
-    # xlsx_file_data.save(f'/Users/evgeniy/Documents/ANKI/{file_name}_cleared.xlsx')
-
     # overwrite source file
-    xlsx_file_data.save(f'/Volumes/big4photo/Documents/ANKI/ANKI_EXCEL.xlsx')
+    # xlsx_file_data.save(anki_excel_file)
 
     messagebox.showinfo(title="All done", message=f'file {file_name}_cleared.xlsx \ncreated')
 
 
 def select_file():  # return path to the file
-    return filedialog.askopenfilename(initialdir='/Users/evgeniy/Documents/ANKI/', defaultextension='xlsx')
+    return filedialog.askopenfilename(initialdir=(Path().home() / 'Library/Mobile Documents/com~apple~CloudDocs/ANKI'),
+                                      defaultextension='xlsx')
 
 
 def get_filename(file_path):
@@ -51,4 +59,4 @@ def get_filename(file_path):
 
 if __name__ == '__main__':
     # clear_from_tags(select_file())
-    clear_from_tags("/Volumes/big4photo/Documents/ANKI/ANKI_EXCEL.xlsx")
+    clear_from_tags((Path().home() / 'Library/Mobile Documents/com~apple~CloudDocs/ANKI/All Decks_cleared.xlsx'))
